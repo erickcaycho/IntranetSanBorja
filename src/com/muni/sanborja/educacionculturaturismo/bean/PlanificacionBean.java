849eee8c30cd592f84d73743ecad6d3c9f708eb6
Erick Caycho <ecaycho7@gmail.com>
2017-09-19 17:55:37 -0500

    Estilo de página, ok. Llenado de tabla principal, ok

--- src/com/muni/sanborja/educacionculturaturismo/bean/PlanificacionBean.java
+++ src/com/muni/sanborja/educacionculturaturismo/bean/PlanificacionBean.java
@@ -3,8 +3,10 @@ package com.muni.sanborja.educacionculturaturismo.bean;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import org.apache.log4j.Logger;
 
@@ -41,6 +43,16 @@ public class PlanificacionBean implements Serializable{
 	}
 	public void setSelectedPlanificacion(Planificacion selectedPlanificacion) {
 		SelectedPlanificacion = selectedPlanificacion;
+	}
+	
+	public void eliminarPlanificacion(){
+		log.info("Ingreso de eliminar Planificacion[]");
+		String msg;
+		msg ="Se eliminó correctamente la planificación";
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,msg,null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+		
+		log.info("Eliminado correctamente");
 	}
 	
 }
--- src/com/muni/sanborja/educacionculturaturismo/dao/impl/PlanificacionDaoImpl.java
+++ src/com/muni/sanborja/educacionculturaturismo/dao/impl/PlanificacionDaoImpl.java
@@ -1,12 +1,17 @@
 package com.muni.sanborja.educacionculturaturismo.dao.impl;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import javax.xml.transform.Transformer;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.transform.Transformers;
 
 import com.muni.sanborja.educacionculturaturismo.dao.PlanificacionDao;
 import com.muni.sanborja.educacionculturaturismo.modelo.Planificacion;
@@ -50,8 +55,7 @@ public class PlanificacionDaoImpl implements PlanificacionDao, Serializable {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<PlanificacionPeriodoActividad> listarPlanificacionPeriodoActividad() {
-		
-		List<PlanificacionPeriodoActividad> listaPlanificacionActividadPeriodo = null;
+		List<PlanificacionPeriodoActividad> plan  = null;
 		
 		Session session = HibernateSessionFactory.getSessionFactory().openSession();
 		Transaction ts = session.beginTransaction();
@@ -60,14 +64,31 @@ public class PlanificacionDaoImpl implements PlanificacionDao, Serializable {
 			log.info("inicia");
 			Query query = session.createSQLQuery("CALL obtenerPlanificacionXPeriodoXActividad()");
 			log.info("0");
-			listaPlanificacionActividadPeriodo = query.list();
-			log.info("Registros encontrados: "  + listaPlanificacionActividadPeriodo.size());
+			List<Object[]> objs  = query.list();
 			
-			for(int i=0; i<listaPlanificacionActividadPeriodo.size(); i++){
+			
+			plan = new ArrayList<PlanificacionPeriodoActividad>();
+			for (Object[] o : objs) {
+				Object[] aux = o;
+				PlanificacionPeriodoActividad p = new PlanificacionPeriodoActividad();
 				
-				PlanificacionPeriodoActividad p = (PlanificacionPeriodoActividad)listaPlanificacionActividadPeriodo.get(i);
+				p.setIdPlanificacion((Integer)aux[0]);
+				p.setNomPeriodo((String)aux[1]);
+				p.setFechaCreacion((Date)aux[2]);
+				p.setFechaPlanificacion((Date)aux[3]);
+				p.setFechaAprobacion((Date)aux[4]);
+				p.setFechaRechazo((Date)aux[5]);
+				p.setFechaAnulacion((Date)aux[6]);
+				p.setFechaEjecucion((Date)aux[7]);
+				p.setNomActividad((String)aux[8]);
+				p.setNomTipoActividad((String)aux[9]);
+				p.setEstado((String)aux[10]);
 				
+				plan.add(p);
 			}
+			
+			log.info("Registros encontrados: "  + plan.size());
+			
 			
 			ts.commit();
 			session.close();
@@ -78,7 +99,7 @@ public class PlanificacionDaoImpl implements PlanificacionDao, Serializable {
 		}
 		
 		
-		return listaPlanificacionActividadPeriodo;
+		return plan;
 	}
 
 }
