2f1e1ede79ee3e4d2ab9799a0d897892d9954e66
Erick Caycho <ecaycho7@gmail.com>
2017-09-22 07:16:17 -0500

    Otros cambios en pantalla

---
+++ WebContent/resources/js/planificacion.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	
+	$( "#btnNuevo" ).click(function() {
+		
+	    var nodeName = document
+	       .getElementById('form:tipoactividad_input').nodeName;
+	       alert(nodeName);
+	       
+	       console.log(nodeName);
+
+		
+	});
+
+});
---
+++ src/com/muni/sanborja/educacionculturaturismo/bean/DataService.java
@@ -0,0 +1,11 @@
+package com.muni.sanborja.educacionculturaturismo.bean;
+
+import javax.persistence.*;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public enum DataService {
+
+}
