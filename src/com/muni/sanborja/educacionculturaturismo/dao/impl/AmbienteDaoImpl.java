df12db89fd7faa14ca7854653b5f949c0634857d
KGil <KGil@KGIL.belltechla.com>
2017-09-26 02:22:35 -0500

    Cambios Karen 2

---
+++ src/com/muni/sanborja/educacionculturaturismo/dao/impl/AmbienteDaoImpl.java
@@ -0,0 +1,49 @@
+package com.muni.sanborja.educacionculturaturismo.dao.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.muni.sanborja.educacionculturaturismo.dao.AmbienteDao;
+import com.muni.sanborja.educacionculturaturismo.modelo.Ambiente;
+import com.muni.sanborja.educacionculturaturismo.util.HibernateSessionFactory;
+
+public class AmbienteDaoImpl implements Serializable,AmbienteDao {
+
+	public static Logger log = Logger.getLogger(AmbienteDaoImpl.class);
+	private static final long serialVersionUID = 3558653004795076773L;
+
+	@Override
+	public List<Ambiente> listarAmbiente(int idSede) {
+		List<Ambiente> listaAmbiente = null;
+		Session session = HibernateSessionFactory.getSessionFactory().openSession();
+		Transaction ts = session.beginTransaction();
+		String hql="FROM Ambiente WHERE idsede = :idsede ";
+		
+		try {
+			log.info("Cod Sede " + idSede);
+			
+			listaAmbiente = session.createQuery(hql).setParameter("idsede",idSede ).list();
+			
+			if (listaAmbiente == null) {
+				log.info("No hay Actividades");
+				
+			} else {
+				log.info("[Ambiente] Se encontro " + listaAmbiente.size());
+			}
+			
+			ts.commit();
+			session.close();
+			
+		} catch (Exception e) {
+			log.error("error " +e.getMessage());
+			  e.getMessage();
+		}
+		
+		return listaAmbiente;
+	}
+
+}
