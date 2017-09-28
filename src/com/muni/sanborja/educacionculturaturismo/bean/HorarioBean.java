51a37dca23e7cc0a736367c9d992875791e8a83e
kgeralgil <geraldine_85_13@hotmail.com>
2017-09-27 22:26:32 -0500

    Insert HorarioBean

---
+++ src/com/muni/sanborja/educacionculturaturismo/bean/HorarioBean.java
@@ -0,0 +1,82 @@
+package com.muni.sanborja.educacionculturaturismo.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.apache.log4j.Logger;
+
+import com.muni.sanborja.educacionculturaturismo.dao.HorarioDao;
+import com.muni.sanborja.educacionculturaturismo.dao.impl.HorarioDaoImpl;
+import com.muni.sanborja.educacionculturaturismo.modelo.Horario;
+
+@ManagedBean(name = "horarioBean")
+@SessionScoped
+public class HorarioBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	public static Logger log = Logger.getLogger(PlanificacionActividadBean.class);
+	
+	private List<Horario> listaHorario;
+	private Horario horario,selectedHorario;
+	private String msg;
+	private Date todayDate = new Date();
+
+	@PostConstruct
+	public void init() {
+		horario = new Horario();
+	}
+	
+	public Date getTodayDate() {
+	    return todayDate;
+	}
+	
+	public void crearHorario(){
+		try {
+			
+			log.info("Creado correctamente" + horario.getVacantemax());
+			HorarioDao horarioDao = new HorarioDaoImpl();
+			horarioDao.createHorario(horario);
+			msg ="Se creó correctamente el Horario";
+			
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,msg,null);
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+		catch (Exception e) {
+		log.error("Error:" + e.getMessage());
+		log.error(e.getStackTrace());
+	}
+	}
+
+	public Horario getSelectedHorario() {
+		return selectedHorario;
+	}
+
+	public void setSelectedHorario(Horario selectedHorario) {
+		this.selectedHorario = selectedHorario;
+	}
+
+	public List<Horario> getListaHorario() {
+		return listaHorario;
+	}
+
+	public void setListaHorario(List<Horario> listaHorario) {
+		this.listaHorario = listaHorario;
+	}
+
+	public Horario getHorario() {
+		return horario;
+	}
+
+	public void setHorario(Horario horario) {
+		this.horario = horario;
+	}
+
+}
